4390bd9bca02d493da36332c33bfc933786e2c96
kacpyr23 <49038601+kacpyr23@users.noreply.github.com>
2022-07-06 11:56:38 +0200

    Init project

---
+++ src/main/java/pl/koziol/taskhelper/TaskhelperApplication.java
@@ -0,0 +1,13 @@
+package pl.koziol.taskhelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskhelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskhelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/koziol/taskhelper/TaskhelperApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.koziol.taskhelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskhelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
