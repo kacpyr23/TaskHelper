676222931d51b7726acb5972db6ec2fad520826a
kacpyr23 <49038601+kacpyr23@users.noreply.github.com>
2022-07-06 13:53:44 +0200

    Added database properties

--- src/main/java/pl/koziol/taskhelper/TaskhelperApplication.java
+++ src/main/java/pl/koziol/taskhelper/TaskhelperApplication.java
@@ -1,10 +1,16 @@
 package pl.koziol.taskhelper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class TaskhelperApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(TaskhelperApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(TaskhelperApplication.class, args);
